91b6603cd392ea6c1e4b4cd5da4482bcac368459
tjdgns5272 <69573484+tjdgns5272@users.noreply.github.com>
2021-05-19 21:37:11 +0900

    add waiting room components and change main page

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import styles from './login.module.css';
 
 const Login = () => {
   const history = useHistory();
+  const inputRef = useRef();
 
-  const goToLogin = () => {
-    history.replace('/user/login/set');
+  const handleLogin = (event) => {
+    event.preventDefault();
+    history.push('/user/login/set');
   };
   const goToSignup = () => {
-    history.replace('/users');
+    history.push('/signup');
   };
-
+  const handleOnChange = (event) => {
+    event.preventDefault();
+  };
   return (
     <div className={styles.login}>
-      <form className={styles.login_box}>
+      <h1 className={styles.title}> Play Skatch !</h1>
+      <form ref={inputRef} className={styles.login_box}>
         <div className={styles.input}>
-          <input type="text" placeholder="User name" />
-          <input type="password" placeholder="Password" />
+          <input
+            type="text"
+            placeholder="User Id"
+            name="id"
+            onChange={handleOnChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={handleOnChange}
+          />
         </div>
-        <button type="submit" onClick={goToLogin}>Login</button>
+        <button type="submit" onClick={handleLogin}>Login</button>
       </form>
       <div className={styles.SignUp_Box}>
         <span>If you dont have account, please sign up</span>
---
+++ src/components/user/user.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const User = () => (
+  <li>
+    <div>Character1</div>
+    <div>nickName1</div>
+  </li>
+);
+
+export default User;

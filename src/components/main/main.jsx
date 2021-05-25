688f6d15cd8c3953629bf3647c6412c5441d3278
tjdgns5272 <69573484+tjdgns5272@users.noreply.github.com>
2021-05-25 19:35:59 +0900

    Change main page with Semantic UI

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import SetCharacter from '../setCharacter/setCharacter';
+import {
+  Button, Form, Grid, Icon,
+} from 'semantic-ui-react';
 import styles from './main.module.css';
 
 const Main = () => {
@@ -12,11 +14,11 @@ const Main = () => {
   };
 
   const handleCreateRoom = () => {
-    history.push('/users/new');
+    history.push('/users/play');
     console.log('새로운 방 생성됨');
   };
   const goToRandomPlay = () => {
-    history.push('/users/play');
+    history.push('/users/random');
     console.log('랜덤 플레이 입장했음~');
   };
   const goToLogin = () => {
@@ -24,13 +26,53 @@ const Main = () => {
   };
   return (
     <div className={styles.main}>
-      <SetCharacter />
-      <div className={styles.button_box}>
-        <button type="button" className={styles.button} onClick={goToPrivateRoom}>초대받은 링크로 입장</button>
-        <button type="button" className={styles.button} onClick={handleCreateRoom}>방 만들기</button>
-        <button type="button" className={styles.button} onClick={goToRandomPlay}>랜덤 플레이 하기</button>
-      </div>
-      <button type="button" className={styles.login} onClick={goToLogin}>Login</button>
+      <Grid.Column style={{
+        maxWidth: '50rem',
+        width: '40rem',
+        backgroundColor: 'whitesmoke',
+        padding: '2rem',
+        margin: '1rem',
+      }}
+      >
+        <Form>
+          <Form.Group widths="equal">
+            <Form.Input fluid label="닉네임" placeholder="꼴리는거 쓰셈" />
+            <Form.Field label="캐릭터 선택" control="select">
+              <option defaultValue="character">--------------------------</option>
+              <option value="ryu">조뀨</option>
+              <option value="park">조툰</option>
+              <option value="hyuk">조텩</option>
+              <option value="moon">조텬</option>
+              <option value="hank">조껼</option>
+            </Form.Field>
+          </Form.Group>
+          <Button animated="fade" style={{ marginBottom: '1rem' }}>
+            <Button.Content visible verticalAlign="middle" style={{ width: '33rem' }}>이걸로 확인 버튼 ㄱㄱ</Button.Content>
+            <Button.Content hidden>응 조까~</Button.Content>
+          </Button>
+        </Form>
+        <div className={styles.buttons}>
+          <Button animated color="teal" type="button" className={styles.button} onClick={goToPrivateRoom}>
+            <Button.Content visible>초대받은 링크로 입장</Button.Content>
+            <Button.Content hidden>
+              <Icon style={{ marginBottom: '1rem' }} size="large" name="mail" />
+            </Button.Content>
+          </Button>
+          <Button animated color="grey" type="button" className={styles.button} onClick={handleCreateRoom}>
+            <Button.Content visible>방 만들기</Button.Content>
+            <Button.Content hidden verticalAlign="middle">
+              <Icon style={{ marginBottom: '1rem' }} size="large" name="home" />
+            </Button.Content>
+          </Button>
+          <Button animated color="teal" type="button" className={styles.button} onClick={goToRandomPlay}>
+            <Button.Content visible>랜덤 플레이하기</Button.Content>
+            <Button.Content hidden>
+              <Icon style={{ marginBottom: '1rem' }} size="large" name="random" />
+            </Button.Content>
+          </Button>
+        </div>
+      </Grid.Column>
+      <Button type="button" className={styles.login} onClick={goToLogin}>Login</Button>
     </div>
   );
 };
